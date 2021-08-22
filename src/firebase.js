2a45c498b1fa9759f7da8c93d4665ca5869ae2ce
koutarou <kou06yokohama@gmail.com>
2021-08-22 13:57:27 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChfzBo-B4mtXMf1ORy4pZZqMVSXK9Ypq0",
+  authDomain: "peaches-40b33.firebaseapp.com",
+  projectId: "peaches-40b33",
+  storageBucket: "peaches-40b33.appspot.com",
+  messagingSenderId: "859732748262",
+  appId: "1:859732748262:web:f2c1c0d52c6121d1941827",
+  measurementId: "G-2ZPHHHXWZH",
+}
+
+firebase.initializeApp(firebaseConfig)
