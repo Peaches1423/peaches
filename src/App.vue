0f9caacdbb63e86bb85719853599fb8baa0e01ba
risa-s-92 <r-sato-1d8@eagle.sophia.ac.jp>
2021-08-27 21:34:40 +0900

    firebase途中まで

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,77 @@
 <template>
   <div>
-    <div class="nav_bar">
-      <router-link to="/" class="nav_item nav_link"> タイトル</router-link>
-      <div class="nav_items">
-        <router-link to="/" class="nav_item nav_link">Home</router-link>
-        <router-link to="/" class="nav_item nav_link">ログイン画面</router-link>
+    <div>
+      <div class="nav_bar">
+        <router-link to="/" class="nav_item nav_link">
+          就活情報シェアサイト</router-link
+        >
+        <div class="nav_items">
+          <router-link to="/" class="nav_item nav_link">Home</router-link>
+          <div>
+            <router-link to="/" class="nav_item nav_link"
+              >ログイン画面</router-link
+            >
+          </div>
+        </div>
       </div>
     </div>
-
     <body>
       <h2>投稿フォーム</h2>
       <div class="username">
         <p>ニックネーム</p>
         <textarea class="form_username" v-model="text" />
       </div>
-      <p>日付</p>
-      <p>最近起こった出来事・感想</p>
-      <textarea class="form_textarea" v-model="text" />
+
+      <select name="卒業年度">
+        <option selected>卒業年度</option>
+        <option>21卒以前</option>
+        <option>22卒</option>
+        <option>23卒</option>
+        <option>24卒</option>
+        <option>25卒以降</option>
+      </select>
+      <div class="form">
+        <p>最近起こった出来事・感想</p>
+        <textarea
+          class="form_textarea"
+          placeholder="何でもご自由にどうぞ！"
+          v-model="text"
+        />
+      </div>
       <div class="form_button">
-        <button v-on:click="post" class="form_submit-button">投稿する</button>
+        <button v-on:click="posting" class="form_submit-button">
+          投稿する
+        </button>
       </div>
     </body>
   </div>
 </template>
 
 <script>
+import firebase from "firebase"
 export default {
   data() {
     return {
-      text: "",
+      posts: [],
     }
   },
   methods: {
-    post() {
-      alert("投稿機能の完成をお楽しみに！")
+    posting() {
+      const post = {
+        graduation_year: "卒業年度を入れたい",
+        text: "form_textareaの内容にしたい",
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      }
+      firebase
+        .firestore()
+        .collection("posts")
+        .add(post)
+        .then((ref) => {
+          this.posts.push({
+            id: ref.id,
+            ...post,
+          })
+        })
     },
   },
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase"
 import "firebase/firestore"
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyChfzBo-B4mtXMf1ORy4pZZqMVSXK9Ypq0",
   authDomain: "peaches-40b33.firebaseapp.com",
