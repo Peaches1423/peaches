f6f3661863395cdc5c1e76c8683bf7bb638d714d
Risa-123R <tack.th63@gmail.com>
2021-08-31 10:46:18 +0900

    ログイン機能途中

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+    <div v-if="isLoggin">ログイン中</div>
+    <div v-else>ログインしていません
+    <div @click="login">ログインボタン</div>
+    <div>ログアウト</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+isLoggin: false,
+    },
+  }
+methods:{
+
+    login(){
+      const provider = new firebase.auth.GoogleAuthProvider()
+firebase
+.auth()
+.signInWithPopup(provider)
+.then((result) => {
+console.log({ result })
+if( result.user ) {
+this.isLoggin = true
+}
+  })
+},
+
+</script>
 
 <style>
 #app {
