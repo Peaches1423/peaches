1ba6b19c331d6bfa5cc92bc2db21dd1dfa3792bf
risa-s-92 <r-sato-1d8@eagle.sophia.ac.jp>
2021-09-04 19:54:18 +0900

    投稿フォームのCSSつけた

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="form-page">
     <div>
       <div class="nav_bar">
         <router-link to="/" class="nav_item nav_link">
@@ -20,15 +20,54 @@
       <p>ニックネーム</p>
       <input type="text" v-model="form_username" />
     </div>
-
-    <select v-model="nendo">
-      <option selected>卒業年度</option>
-      <option>21卒以前</option>
-      <option>22卒</option>
-      <option>23卒</option>
-      <option>24卒</option>
-      <option>25卒以降</option>
-    </select>
+    <div>
+      <p>卒業年度</p>
+      <select v-model="nendo">
+        <option>21卒以前</option>
+        <option>22卒</option>
+        <option>23卒</option>
+        <option>24卒</option>
+        <option>25卒以降</option>
+      </select>
+    </div>
+    <div>
+      <p>大学名</p>
+      <input type="text" />
+    </div>
+    <div class="bunri">
+      <p>文理</p>
+      <input type="radio" value="bunkei" />文系
+      <input type="radio" value="rikei" />理系
+    </div>
+    <div>
+      <div>
+        <p>業界</p>
+        <select v-model="gyoukai">
+          <option selected>業界</option>
+          <option>メーカー</option>
+          <option>商社</option>
+          <option>マスコミ</option>
+          <option>その他</option>
+        </select>
+      </div>
+      <div>
+        <p>インターンor本選考</p>
+        <select v-model="internorhonsenkou">
+          <option>インターン</option>
+          <option>本選考</option>
+          <option>その他</option>
+        </select>
+      </div>
+      <div>
+        <p>段階</p>
+        <select v-model="dankai">
+          <option>ES</option>
+          <option>グループディスカッション</option>
+          <option>面接</option>
+          <option>その他</option>
+        </select>
+      </div>
+    </div>
     <div class="form">
       <p>最近起こった出来事・感想</p>
       <textarea placeholder="何でもご自由にどうぞ！" v-model="form_textarea" />
@@ -63,12 +102,18 @@ export default {
 </script>
 
 <style>
+.form-page {
+  background: url(../images/peaches.jpeg);
+  background-size: 100% auto;
+}
+
 .nav_bar {
   height: 50px;
   display: flex;
   justify-content: space-between;
   align-items: stretch;
-  background-color: aqua;
+  background-color: #492f10;
+  opacity: 0.7;
 }
 
 .nav_link {
@@ -81,12 +126,13 @@ export default {
 
 .username {
   display: flex;
+  height: 20px;
 }
 .form_username {
   height: 15px;
 }
-.form_textarea {
+.form textarea {
   height: 10em;
-  width: 30em;
+  width: 50em;
 }
 </style>
