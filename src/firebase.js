26b0f9eeb9f3d491d6384994a3d041bcf3f80a1e
Risa-123R <tack.th63@gmail.com>
2021-09-04 20:13:53 +0900

    ログイン機能変更中

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import firebase from "firebase"
 import "firebase/firestore"
 import "firebase/auth"
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcSXTfTQ6rsYu6NS467aCDctg2b7niP0w",
-  authDomain: "webexpert-demo-project.firebaseapp.com",
-  projectId: "webexpert-demo-project",
-  storageBucket: "webexpert-demo-project.appspot.com",
-  messagingSenderId: "321948403209",
-  appId: "1:321948403209:web:2e2689356ff070564e1ad8",
+  apiKey: "AIzaSyChfzBo-B4mtXMf1ORy4pZZqMVSXK9Ypq0",
+  authDomain: "peaches-40b33.firebaseapp.com",
+  projectId: "peaches-40b33",
+  storageBucket: "peaches-40b33.appspot.com",
+  messagingSenderId: "859732748262",
+  appId: "1:859732748262:web:f2c1c0d52c6121d1941827",
+  measurementId: "G-2ZPHHHXWZH",
 }
 
 firebase.initializeApp(firebaseConfig)
